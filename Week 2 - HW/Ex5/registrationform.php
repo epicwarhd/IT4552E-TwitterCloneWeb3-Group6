287501a09c4eff16dbe8d3cf0224201bfea2e5f3
LeThanhHai <epicwarhd@gmail.com>
2022-04-12 19:33:40 +0700

    Homework

---
+++ Week 2 - HW/Ex5/registrationform.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>A Simple Form</title>
+    </head>
+
+    <body>
+        <form action="http://fit.hut.edu.vn/~trangntt/courses/vp" method="POST">
+            Sign up your gmail
+            <br>
+            Gmail: <input type="text" name="Gmail">
+            <br>
+            Password: <input type="password" name="Password">
+            <br>
+            Confirm password: <input type="password" name="PasswordConfirm">
+            <br>
+            <input type="submit" value="Click To Submit">
+            <br>
+            <input type="reset" value="Erase and Restart">
+        </form>
+    </body>
+</html>
