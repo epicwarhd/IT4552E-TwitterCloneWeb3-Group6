734580ed89189697315fdce6cfa76162fcea290c
LeThanhHai <epicwarhd@gmail.com>
2022-06-07 22:48:22 +0700

    Homework

---
+++ Homework/Week 8 - HW/framework/config/config.php
@@ -0,0 +1,18 @@
+<?php
+    
+    /** Configuration Variables **/
+    
+    const DEVELOPMENT_ENVIRONMENT = true;
+    
+
+
+const DB_NAME = 'framework';
+//    const DB_NAME = 'framework';
+const DB_USER = 'dangvh';
+const DB_PASSWORD = 'Vuhaidang157@';
+const DB_HOST = 'localhost';
+    
+    const BASE_PATH = 'http://dangvh.com/webprog/Lab08/framework';
+    
+    
+    const PAGINATE_LIMIT = '5';
